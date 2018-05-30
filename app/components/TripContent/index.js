eed6e215d89fb30037c6be08cc4037cef3e572e5
Pasi Lampinen <plam@fl027.local>
2018-05-30 23:16:18 +0300

    background color dark for trip info page images

--- app/components/TripContent/index.js
+++ app/components/TripContent/index.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {
-  StyleSheet,
-  View,
-  Linking,
-  Image,
-  Dimensions,
-  Platform,
-} from 'react-native';
+import { StyleSheet, View, Linking, Image, Dimensions, Platform } from 'react-native';
 import ParsedText from 'react-native-parsed-text';
 import theme from '../../style/theme';
 
@@ -42,26 +35,28 @@ const styles = StyleSheet.create({
     width,
     height: 240,
     maxHeight: 240,
-  }
+    backgroundColor: theme.darker,
+  },
 });
 
 const TripContent = ({ venue, children }) => {
   return (
     <View style={styles.container}>
-
-      <Text style={styles.title} bold>{venue.get('title')}</Text>
+      <Text style={styles.title} bold>
+        {venue.get('title')}
+      </Text>
       <Image source={{ uri: venue.get('imageUrl') }} style={styles.image} resizeMode="cover" />
 
       <ParsedText
         style={styles.text}
-        parse={[{ type: 'url', style: styles.url, onPress: (url) => Linking.openURL(url) }]}
+        parse={[{ type: 'url', style: styles.url, onPress: url => Linking.openURL(url) }]}
       >
         {venue.get('subtitle')}
       </ParsedText>
 
       {children}
     </View>
-  )
-}
+  );
+};
 
 export default TripContent;
